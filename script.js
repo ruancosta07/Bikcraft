110b7bdb1a73ffb208327ec163a332cb26331fae
RuanCosta07 <ruancosta.ti0805@gmail.com>
2023-10-12 13:40:42 -0300

    animação ao enviar formulário

--- script.js
+++ script.js
@@ -133,3 +133,31 @@ mostraElementos()
 window.addEventListener('scroll', ()=>{
     mostraElementos()
 })
+
+const formulario = document.querySelector('form')
+const formularioEnviadoSucesso = document.getElementById('formulario-sucesso')
+const barraInferiorFormulario = document.getElementById('formulario-sucesso-underline')
+
+function enviarFormulario(evento){
+    evento.preventDefault()
+    const botao = document.querySelector('form button')
+    botao.disabled = true
+    botao.innerText = 'Enviando...'
+    setTimeout(()=>{
+        botao.innerText = 'Enviado!'
+        formularioEnviadoSucesso.classList.toggle('hidden')
+        setTimeout(()=>{
+            formularioEnviadoSucesso.classList.toggle('translate-x-[100%]')
+            formularioEnviadoSucesso.classList.toggle('opacity-0')
+            barraInferiorFormulario.classList.add('translate')
+        }, 100)
+        setTimeout(()=>{
+            formularioEnviadoSucesso.classList.toggle('translate-x-[100%]')
+            formularioEnviadoSucesso.classList.toggle('opacity-0')
+        }, 5000)
+    }, 2000)
+}
+
+
+
+formulario.addEventListener('submit', enviarFormulario)
